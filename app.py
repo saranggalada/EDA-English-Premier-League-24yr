bce954831d7d3319dae0ae852253230f9e28f359
saranggalada <sarang.galada@gmail.com>
2024-01-12 15:35:31 +0530

    Updated points calculation

--- app.py
+++ app.py
@@ -240,8 +240,6 @@ def get_league_table(SeasonPoints, SeasonWins, SeasonDraws, SeasonLoss, SeasonGo
     table = table.set_index('Club')
     table = table.sort_values(by=['Pts','GD','GF'], ascending=False)
     table['Pos'] = pd.Series(range(1,21), index=table.index)
-    table = table.set_index('Pos')
-    # table = table[['Pos','Club','MP','W','D','L','GF','GA','GD','Pts']]
     
     return table
 
